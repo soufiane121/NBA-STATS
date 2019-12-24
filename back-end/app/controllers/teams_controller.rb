db73cb659ec24606a93d4dff238848ba2de56876
soufianeou <soufiane.oucherrou@gmail.com>
2019-12-24 10:13:31 -0500

    logos

--- back-end/app/controllers/teams_controller.rb
+++ back-end/app/controllers/teams_controller.rb
@@ -2,8 +2,18 @@ class TeamsController < ApplicationController
 
 
     def index
-        @teams= Team.all_teams
-        render json: @teams
+        # # @teams= Team.all_teams
+        # @teams = Team.create_all_teams
+        # render json: @teams
+        # if Team.create_all_teams.length == 0
+        #     @teams = Team.create_all_teams
+        #       render json: @teams 
+        # else
+        #     @teams = Team.all_teams
+        #       render json: @teams
+        # end
+        teams = Team.all_teams
+        render json: teams
     end
 
     
