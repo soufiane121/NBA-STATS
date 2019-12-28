ed384e76e7a1f968c7cfe75c82e1dbf551fcd143
soufianeou <soufiane.oucherrou@gmail.com>
2019-12-27 20:30:58 -0500

    second chart

--- front-end/src/components/PortFolio.js
+++ front-end/src/components/PortFolio.js
@@ -4,10 +4,41 @@
  class PortFolio extends React.Component {
 
    state={
-     options:{},
+     options:{
+       chart:{color: 'f3f3f3'},
+       labels: ["Wins", "Losses"],
+       title:{text:`Games played ${this.props.singleTeam.Games}`},
+      //  responsive: [{breakpoint: 480}]
+      colors: ['#1ab7ea', '#0084ff', '#39539E', '#0077B5'],
+      },
      series: [this.props.singleTeam.Wins, this.props.singleTeam.Losses],
+     optionsRebound:{
+      plotOptions: {
+        radialBar: {
+          endAngle: 270,
+          hollow: {
+            size: '30%',  }, } },
+
+            colors: ['#1ab7ea', '#0084ff', '#39539E', '#0077B5'],
+            labels: ['Offensive Rebounds', 'DefensiveRebounds', 'Rebounds'],
+
+            legend: {
+              show: true,
+              floating: true,
+              fontSize: '16px',
+              position: 'left',
+              offsetX: 160,
+              offsetY: 10, },
+     },
+     seriesRebound: [this.props.singleTeam.OffensiveRebounds, 
+      this.props.singleTeam.DefensiveRebounds,
+      this.props.singleTeam.Rebounds
+    ],
    }
     
+//    OffensiveRebounds: 250.4
+// DefensiveRebounds: 837.9
+// Rebounds: 1088.3
 
     render(){
         return(
@@ -19,8 +50,17 @@
                 options={this.state.options}
                 series={this.state.series}
                 type="donut"
-                width="300"
-                
+                width="500"
+              />
+             </div>
+           </div>
+           <div className="row2">
+             <div className="mixed-chart2">
+             <Chart 
+              options={this.state.optionsRebound}
+              series={this.state.seriesRebound}
+              type="radialBar"
+              height="500"
               />
              </div>
            </div>
